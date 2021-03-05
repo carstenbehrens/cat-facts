03e517b2bf5630b8c4cd4b03231b6ffbe59b003e
Carsten Behrens <behrens.carsten@guj.de>
2021-03-05 19:33:13 +0100

    Change font color on BreedComponent

--- src/components/BreedComponent.vue
+++ src/components/BreedComponent.vue
@@ -57,6 +57,7 @@ export default {
   max-width: 400px;
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  color: #444d56;
 }
 
 .breed-container:hover {
