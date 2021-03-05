b61ddcc43e5088e772e38e8dfa1e1801cf8f544b
Carsten Behrens <behrens.carsten@guj.de>
2021-03-05 18:11:23 +0100

    Remove duplicate code

--- tests/unit/services/catService.spec.js
+++ tests/unit/services/catService.spec.js
@@ -2,13 +2,14 @@ import CatService from "../../../src/services/catService";
 
 describe("#catService", () => {
   const catService = new CatService();
+  beforeEach(() => {
+    global.fetch = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ json: () => {} }));
+  });
 
   describe("#get breed", () => {
     test("calls the API with the default query params", async () => {
-      global.fetch = jest
-        .fn()
-        .mockImplementation(() => Promise.resolve({ json: () => {} }));
-
       await catService.getBreeds();
       expect(global.fetch).toHaveBeenCalledWith(
         "https://catfact.ninja/breeds?limit=1&page=1"
@@ -16,10 +17,6 @@ describe("#catService", () => {
     });
 
     test("calls the API with the correct query params", async () => {
-      global.fetch = jest
-        .fn()
-        .mockImplementation(() => Promise.resolve({ json: () => {} }));
-
       await catService.getBreeds(3, 12);
       expect(global.fetch).toHaveBeenCalledWith(
         "https://catfact.ninja/breeds?limit=3&page=12"
